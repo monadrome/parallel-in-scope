6abcbb121c107f84eadcae0793b5540fe1f584df
EricLin <linqinghua4@gmail.com>
2026-09-25 04:14:33 -0400

    style: drop unused imports in TaskGraphPolarityTest

--- src/test/java/io/github/monadrome/parallelinscope/TaskGraphPolarityTest.java
+++ src/test/java/io/github/monadrome/parallelinscope/TaskGraphPolarityTest.java
@@ -4,8 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.Duration;
 import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.jupiter.api.AfterEach;
